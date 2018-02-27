efa87dcf8d814a6a6d17cea37fa602580cde8c9a
Silatibebe <bereketyoseph2013@gmail.com>
2018-02-26 20:48:29 -0800

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
+
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(bodyParser.json());
@@ -62,11 +64,18 @@ app.get('/todos',(req,res) =>{
     )
 });
 
-//delete request
-app.delete('/todos/:id', (req,res) =>{
+//patch/update request
+app.patch('/todos/:id', (req,res) =>{
     var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
     if(!ObjectID.isValid(id)) return res.status(404).send();
-    Todo.findByIdAndRemove(id).then((todo) => {
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set:body},{new: true}).then((todo) => {
             if(todo) {
               res.send({todo});
             }
@@ -74,6 +83,9 @@ app.delete('/todos/:id', (req,res) =>{
         })
     .catch((e) =>res.status(400).send());
 });
+
+
+
 
 app.listen(port, () =>{
     console.log(`server started on port ${port}`);
